db71527f26cfa8cb7575a3c7af1b3844b64d09c2
i6448038 <376832293@qq.com>
2017-05-23 16:42:11 +0800

    json problem is ok

--- main.go
+++ main.go
@@ -7,27 +7,121 @@ import (
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
+	"github.com/fatih/color"
+	"solr_console/utils"
 )
 
-func main() {
-	app := cli.NewApp()
+var app *cli.App
+
+func init(){
+	app = cli.NewApp()
 	app.Name = "solr_console"
 	app.Usage = "This is a solr console management tool"
-	app.Action = func(c * cli.Context) error {
-		fmt.Println("welcome to the solr management platform")
-
-		version, err := getSolrVersion("lucene")
-
-		if err != nil {
-			fmt.Println("出现了错误！", err.Error())
-			return err
-		}
-		fmt.Println("Solr的版本为:", version)
-
-		return nil
-	}
-	app.Run(os.Args)
+	initFlags()
+	initCommands()
 }
+
+
+
+func main() {
+	app.Run(os.Args)
+
+}
+
+
+//func initAppFlags(){
+//	app.Flags = []cli.Flag{
+//		cli.StringFlag{
+//			Name: "config, c",
+//		    Usage: "Load solr configuration from `FILE`",
+//		},
+//	}
+//}
+//
+//func initActions(){
+//	app.Action = func(c * cli.Context) error {
+//		color.Yellow("welcome to the solr management platform!")
+//
+//		version, err := getSolrVersion("lucene")
+//
+//		if err != nil {
+//			color.Red("出现了错误！" + err.Error())
+//			return err
+//		}
+//
+//		if v, ok := version.(string); ok{
+//			version = v
+//			color.Yellow("当前Solr的版本为:" + v)
+//		}
+//
+//
+//
+//		return nil
+//	}
+//}
+
+func initCommands(){
+	app.Commands = []cli.Command{
+		{
+			Name: "add",
+			Usage: "add a new document or some documents",
+			Aliases: []string{"c"},
+			Description: "the c of the `CURD`,insert a new data into the solr core",
+			ArgsUsage: `solr的格式为json字符串或者字符串数组（例如: {"id": 22, "title": "abc"}
+			或者：[{"id": 22, "title": "abc"}, {"id": 23, "title": "def"}, {"id": 24, "title": "def"}]）` ,
+			Action: func (c *cli.Context)error {
+				resp := utils.Create(c.Args().First())
+				if(resp != nil){
+					color.Blue(resp.String())
+				}
+				return nil
+			},
+		},
+		{
+			Name: "update",
+			Usage: "update a document or some documents",
+			Aliases: []string{"u"},
+			Description: "the u of the `CURD`,update documents",
+			ArgsUsage: `solr的格式为json字符串或者字符串数组（例如: {"id": 22, "title": "abc"}
+			或者：[{"id": 22, "title": "abc"}, {"id": 23, "title": "def"}, {"id": 24, "title": "def"}]）` ,
+			Action: func (c *cli.Context)error {
+				color.Yellow(c.Args().First())
+				resp := utils.Update(c.Args().First())
+				if(resp != nil){
+					color.Blue(resp.String())
+				}
+				return nil
+			},
+		},
+		{
+			Name: "delete",
+			Usage: "delete a document or some documents",
+			Aliases: []string{"d"},
+			Description: "the d of the `CURD`,delete documents",
+			Action: func (c *cli.Context)error {
+				color.Red(c.Args().First())
+				resp := utils.Delete(c.Args().First())
+				if(resp != nil){
+					color.Blue(resp.String())
+				}
+				return nil
+			},
+		},
+	}
+}
+
+
+func initFlags(){
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "lang, l",
+			Value: "english",
+			Usage: "language for the greeting",
+			EnvVar: "LEGACY_COMPAT_LANG,APP_LANG,LANG",
+		},
+	}
+}
+
 
 func getSolrVersion(key string)(version interface{}, err error) {
 	host := "localhost"
